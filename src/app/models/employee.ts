d8af7b2d96599d8dde6d2dbe326368774bcb7872
Hammad Baig <hammadbaig862@gmail.com>
2022-07-07 09:35:57 +0500

    first commit

---
+++ src/app/models/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    idNo: number,
+    employee_name: string,
+    date: Date,
+    project: string,
+    task: string,
+    hours: number,
+    status: string,
+    comments: string
+}
